0a97ef3fbe887717769e799bbe24d46acc94de12
Oleg Korneev <waterpoleg@gmail.com>
2021-12-08 21:55:21 +0300

    7. ListIterator [#350217 #237456] - refactor

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,54 +7,41 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> i = list.listIterator();
-        while (i.hasNext()) {
-            if (i.nextIndex() == index) {
-                i.add(value);
-                break;
-            }
-            i.next();
-        }
+        ListIterator<T> listIterator = list.listIterator(index);
+        listIterator.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        var i = list.listIterator();
-        while (i.hasNext()) {
-            if (i.nextIndex() == index) {
-                i.next();
-                i.add(value);
-                break;
-            }
-            i.next();
-        }
+        var listIterator = list.listIterator(index + 1);
+        listIterator.add(value);
     }
 
     public static <T> List<T> removeIf(List<T> list, Predicate<T> filter) {
-        var i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.remove();
+        var listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (filter.test(listIterator.next())) {
+                listIterator.remove();
             }
         }
         return list;
     }
 
     public static <T> List<T> replaceIf(List<T> list, Predicate<T> filter, T value) {
-        var i = list.listIterator();
-        while (i.hasNext()) {
-            if (filter.test(i.next())) {
-                i.set(value);
+        var listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (filter.test(listIterator.next())) {
+                listIterator.set(value);
             }
         }
         return list;
     }
 
     public static <T> List<T> removeAll(List<T> list, List<T> elements) {
-        var i = list.listIterator();
-        while (i.hasNext()) {
-            if (elements.contains(i.next())) {
-                i.remove();
+        var listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            if (elements.contains(listIterator.next())) {
+                listIterator.remove();
             }
         }
         return list;

975a9e4f79c7c116fe6361e38580543338b56f24
Oleg Korneev <waterpoleg@gmail.com>
2021-12-22 15:47:09 +0300

    2. Анализ доступности сервера. [#859 #242458] - refactor

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,34 +1,22 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> list = new ArrayList<>();
-        List<String> downtime = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             String line;
+            StringBuilder tmp = null;
             while ((line = in.readLine()) != null) {
-                list.add(line);
+                String[] times = line.split(" ");
+                if (("400".equals(times[0]) || "500".equals(times[0])) && tmp == null) {
+                    tmp = new StringBuilder(times[1] + ";");
+                } else if (("200".equals(times[0]) || "300".equals(times[0])) && tmp != null) {
+                    out.println(tmp.append(times[1]).append(";"));
+                    tmp = null;
+                }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        StringBuilder tmp = null;
-        for (var line : list) {
-            String[] times = line.split(" ");
-            if (("400".equals(times[0]) || "500".equals(times[0])) && tmp == null) {
-                tmp = new StringBuilder(times[1] + ";");
-            } else if (("200".equals(times[0]) || "300".equals(times[0])) && tmp != null) {
-                tmp.append(times[1]).append(";");
-                downtime.add(tmp.toString());
-                tmp = null;
-            }
-        }
-        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            downtime.forEach(out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }

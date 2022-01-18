53b1bb1127c4f7562710c2e61ff1b85dfb43020f
Oleg Korneev <waterpoleg@gmail.com>
2022-01-18 21:54:10 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,6 +3,7 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -15,8 +16,14 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
-                        if (str.contains("msg=Bye")) {
+                        if (str.contains("msg=Hello")) {
+                            out.write("Hello".getBytes(StandardCharsets.UTF_8));
+                        } else if (str.contains("msg=Exit")) {
                             server.close();
+                        } else {
+                            if (str.contains("msg=")) {
+                                out.write("What".getBytes(StandardCharsets.UTF_8));
+                            }
                         }
                     }
                     out.flush();

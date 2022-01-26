994a0344bf2c0aa20806fafd94f6a819adeae44f
Oleg Korneev <waterpoleg@gmail.com>
2022-01-26 20:32:42 +0300

    add space

--- data/sql/fauna.sql
+++ data/sql/fauna.sql
@@ -13,7 +13,7 @@ insert into fauna (name, avg_age, discovery_date) values ('cow', 25000, '1917-12
 insert into fauna (name, avg_age, discovery_date) values ('bull', 19000, '2005-07-15');
 insert into fauna (name, avg_age, discovery_date) values ('human', 2000000, '1937-02-03');
 
-select * from fauna where name like'%fish%';
+select * from fauna where name like '%fish%';
 select * from fauna where avg_age between 10000 and 21000;
 select * from fauna where discovery_date is null;
 select * from fauna where discovery_date < '1950.01.01';

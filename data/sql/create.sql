3d1f9f026d98acf8125df8cab10d101fa38703c1
Oleg Korneev <waterpoleg@gmail.com>
2022-01-26 16:23:02 +0300

    1. Схема прав пользователи и роли. [#1093 #256039] - fix, add ";"

--- data/sql/create.sql
+++ data/sql/create.sql
@@ -1,34 +1,34 @@
 create table role(
 	id serial primary key,
 	name varchar(255)
-)
+);
 
 create table users(
 	id serial primary key,
 	name varchar(255),
 	role_id int references role(id)
-)
+);
 
 create table rules(
 	id serial primary key,
 	name varchar(255)
-)
+);
 
 create table role_rules(
 	id serial primary key,
 	role_id int references role(id),
 	rules_id int references rules(id)
-)
+);
 
 create table state(
 	id serial primary key,
 	name varchar(255)
-)
+);
 
 create table category(
 	id serial primary key,
 	name varchar(255)
-)
+);
 
 create table item(
 	id serial primary key,
@@ -36,19 +36,19 @@ create table item(
 	users_id int references users(id),
 	category_id int references category(id),
 	state_id int references state(id)
-)
+);
 
 create table comments(
 	id serial primary key,
 	name varchar(255),
 	item_id int references item(id)
-)
+);
 
 create table attaches(
 	id serial primary key,
 	name varchar(255),
 	item_id int references item(id)
-)
+);
 
 
 

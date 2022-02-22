0eb5886040c9afacd435385e53242c93f72005f0
Oleg Korneev <waterpoleg@gmail.com>
2022-02-22 18:26:56 +0300

    3. Шаблонизатор. [#855]

---
+++ src/main/java/ru/job4j/tdd/template/TemplateGenerator.java
@@ -0,0 +1,10 @@
+package ru.job4j.tdd.template;
+
+import java.util.Map;
+
+public class TemplateGenerator implements Generator {
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/job4j/tdd/template/GeneratorTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.tdd.template;
+
+import junit.framework.TestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest extends TestCase {
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenManyKeys() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = Map.of("name", "Martin", "subject", "you", "age", "50");
+        Generator generator = new TemplateGenerator();
+        generator.produce(template, map);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNotEnoughKeys() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = Map.of("name", "Martin");
+        TemplateGenerator generator = new TemplateGenerator();
+        generator.produce(template, map);
+    }
+
+    @Ignore
+    @Test
+    public void whenDataIsValid() {
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = Map.of("name", "Fowler", "subject", "you");
+        TemplateGenerator generator = new TemplateGenerator();
+        assertThat(generator.produce(template, map), is("I am a Fowler, Who are you?"));
+    }
+}
